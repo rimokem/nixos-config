c4b2451ae18d30add694614804c7e882dc292bb8
Kento Sakaguchi <kent.sk.815@gmail.com>
2024-03-15 12:08:39 +0900

    fix

--- home/shell/nushell/default.nix
+++ home/shell/nushell/default.nix
@@ -2,6 +2,11 @@
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
-    envFile.source = ./env.nu;
+    # envFile.source = ./env.nu;
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
   };
 }
