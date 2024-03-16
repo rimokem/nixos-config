80418f0a336240350dca573e1f46fd9b4f9d5fe2
Kento Sakaguchi <kent.sk.815@gmail.com>
2024-03-17 00:17:48 +0900

    add tree alias

--- home/shell/nushell/default.nix
+++ home/shell/nushell/default.nix
@@ -3,6 +3,9 @@
     enable = true;
     configFile.source = ./config.nu;
     # envFile.source = ./env.nu;
+    shellAliases = {
+      tree = "eza --tree --icons --git";
+    };
   };
 
   programs.carapace = {
